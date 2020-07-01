ba3600de816beba9a08c43089e68eb630e4ccc45
DongKeun1 <skj2366@naver.com>
2020-07-01 17:32:48 +0900

    make a project, first commit

---
+++ src/main/java/com/estella/skj/EstellaGramoApplication.java
@@ -0,0 +1,13 @@
+package com.estella.skj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstellaGramoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstellaGramoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estella/skj/EstellaGramoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estella.skj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstellaGramoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
